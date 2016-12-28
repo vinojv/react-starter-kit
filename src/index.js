8520bd827e662b48e4679bd285f994683f9d4f90
Nighitha <nighitha.kuriachan@razorthink.net>
2016-12-28 20:58:00 +0530

    todo_list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'React';
+import React, {Component} from 'react';
 import {render} from 'react-dom';
+import style from './index.css';
 
 class App extends Component {
   constructor(props, context){
@@ -8,9 +9,22 @@ class App extends Component {
   }
 
   render(){
+    let todoItems = ["Get Nikitha to do JS tasks", "Designs from sandeep", "Work on Axis app"]
     return (
-      <div>
-        HERE YOUR HTML GOES
+      <div className= {style.page__wrapper}>
+      <div className={style.text__content}>
+        <input type="text" className={style.input__text}/>
+        <button type="submit" className={style.button}>Submit</button></div>
+        <div className = {style.content}>
+        <ul className = {style.list}>
+
+          {todoItems.map(item=><li key= {item} className = {style.list__item}>
+          
+            <div className={style.close__icon}>{item}<span>x</span></div>
+
+          </li>)}
+        </ul>
+      </div>
       </div>
       )
   }
