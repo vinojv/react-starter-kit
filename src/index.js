3adb98ba420f1c59ae0cf17fbfb9ffe4ae78d69a
Nighitha <nighitha.kuriachan@razorthink.net>
2017-01-12 09:57:29 +0530

    react-js_main

--- src/index.js
+++ src/index.js
@@ -2,36 +2,80 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import style from './index.css';
 
+const Header = ()=> <div className={style.heading}>ToDo Item</div>
+const Footer = ()=>
+(
+      <div className={style.footer}>
+            <span className={style.footer__text}>&copy copyright</span>
+      </div>)
+ 
+const Navigation=()=>
+  (
+     <div className={style.content__aside}>
+
+        <ul className={style.content__aside__list}>
+            <li className={style.content__aside__list__item}>item__1 
+            </li>
+            <li className={style.content__aside__list__item}>item__2
+            </li>
+            <li className={style.content__aside__list__item}>item__3 
+            </li>
+        </ul>
+    </div>
+  )
+
+  const Content=({ todoItems })=>
+(        
+  <div className={style.content__main}>
+    <div className={style.content__body}>
+      <div className={style.text__content}>
+        <input type="text" value={"display"} id="inputid" className={style.input__text}/>
+       
+        <button type="submit" id="buttonid" className={style.button}>Submit</button>
+      </div>
+      <div className = {style.content}>
+        <ul className = {style.list}>
+           {todoItems.map(item=><li key= {item} id="listid" className = {style.list__item}>
+           <div className={style.close__icon}>{item}<span className={style.close__icon__tag}>x</span></div>
+            </li>)}
+        </ul>
+    </div>
+    </div>
+  </div>
+)
+
+
 class App extends Component {
   constructor(props, context){
     super(props, context);
 
-  }
+    this.state = {
+      todoItems: ["Get Nikitha to do JS tasks", "Designs from sandeep", "Work on Axis app"]
+    }
+    //this.state = {
+     // display: ["hello"]
+    //}
 
+  };
   render(){
-    let todoItems = ["Get Nikitha to do JS tasks", "Designs from sandeep", "Work on Axis app"]
     return (
       <div className= {style.page__wrapper}>
-      <div className={style.text__content}>
-        <input type="text" className={style.input__text}/>
-        <button type="submit" className={style.button}>Submit</button></div>
-        <div className = {style.content}>
-        <ul className = {style.list}>
+          <Header></Header>
+          <div className={style.content__wrapper}>
+             
+            <Content 
+               addTo = {function(todoItems){
+                // todoItems.push(item)
+               }}
 
-          {todoItems.map(item=><li key= {item} className = {style.list__item}>
-          
-            <div className={style.close__icon}>{item}<span>x</span></div>
-
-          </li>)}
-        </ul>
+              todoItems = {this.state.todoItems}></Content>
+          </div>
+          <Footer></Footer>
       </div>
-      </div>
-      )
+  )}
   }
 
-};
 
 render(
     <App/>,
-  document.getElementById('root')
-)
+    document.getElementById('root')
